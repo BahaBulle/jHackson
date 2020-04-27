8a3ca9f6b037037636f426757b4edf51d3e1b834
BahaBulle <bahabulle@hotmail.com>
2020-04-27 22:22:14 +0200

    Add licence and reference to author of SDD1 algorithm

--- Plugins/jHackson.StarOcean/Compression/SDD1_OL.cs
+++ Plugins/jHackson.StarOcean/Compression/SDD1_OL.cs
@@ -1,4 +1,34 @@
-﻿using System;
+﻿/************************************************************************
+
+S-DD1'algorithm emulation code
+------------------------------
+
+Author:	     Andreas Naive
+Date:        August 2003
+Last update: October 2004
+
+This code is Public Domain. There is no copyright holded by the author.
+Said this, the author wish to explicitly emphasize his inalienable moral rights
+over this piece of intelectual work and the previous research that made it
+possible, as recognized by most of the copyright laws around the world.
+
+This code is provided 'as-is', with no warranty, expressed or implied.
+No responsability is assumed by the author in connection with it.
+
+The author is greatly indebted with The Dumper, without whose help and
+patience providing him with real S-DD1 data the research would have never been
+possible. He also wish to note that in the very beggining of his research,
+Neviksti had done some steps in the right direction. By last, the author is
+indirectly indebted to all the people that worked and contributed in the
+S-DD1 issue in the past.
+
+An algorithm's documentation is available as a separate document.
+The implementation is obvious when the algorithm is
+understood.
+
+************************************************************************/
+
+using System;
 using System.IO;
 
 namespace jHackson.StarOcean.Compression
