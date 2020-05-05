3b937a843c66ee6ffa0b7f981e49bf7ca76657a5
BahaBulle <bahabulle@hotmail.com>
2020-05-05 21:58:55 +0200

    Add static method to peek a byte in BinaryReader

---
+++ Plugins/jHackson.StarOcean/Extensions/BinaryReaderExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace jHackson.StarOcean.Extensions
+{
+    public static class BinaryReaderExtension
+    {
+        public static byte PeekByte(this BinaryReader reader)
+        {
+            var pos = reader.BaseStream.Position;
+
+            var value = reader.ReadByte();
+
+            reader.BaseStream.Position = pos;
+
+            return value;
+        }
+
+        public static byte PeekByte(this BinaryReader reader, int posToAdd)
+        {
+            var pos = reader.BaseStream.Position;
+
+            reader.BaseStream.Position += posToAdd;
+
+            var value = reader.ReadByte();
+
+            reader.BaseStream.Position = pos;
+
+            return value;
+        }
+    }
+}
