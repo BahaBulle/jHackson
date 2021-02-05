15a5f2e508d813506cb962ea14937e129ff1b466
BahaBulle <bahabulle@gmail.com>
2021-02-05 22:12:00 +0100

    CLEAN : Modify RootNamespace

--- jHackson.Core/Localization/Resources/CoreResource.Designer.cs
+++ jHackson.Core/Localization/Resources/CoreResource.Designer.cs
@@ -39,7 +39,7 @@ namespace JHackson.Core.Localization.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("jHackson.Core.Localization.Resources.CoreResource", typeof(CoreResource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("JHackson.Core.Localization.Resources.CoreResource", typeof(CoreResource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
