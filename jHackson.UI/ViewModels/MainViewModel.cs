92212218befe2f8f9c5eea34a88fb04d68d88c95
BahaBulle <bahabulle@gmail.com>
2022-01-13 22:52:34 +0100

    UI : Create main view and add Quit button

---
+++ jHackson.UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="MainViewModel.cs" company="BahaBulle">
+// Copyright (c) BahaBulle. All rights reserved.
+// </copyright>
+
+namespace JHackson.UI.ViewModels
+{
+    using System.Windows;
+    using Prism.Commands;
+
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.QuitCommand = new DelegateCommand(this.Quit);
+        }
+
+        public DelegateCommand QuitCommand { get; private set; }
+
+        private void Quit()
+        {
+            Application.Current.MainWindow.Close();
+        }
+    }
+}
