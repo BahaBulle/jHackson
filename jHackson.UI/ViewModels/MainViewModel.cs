cc2b8ad34fbbe1578fbce1d596b605847a9934e4
BahaBulle <bahabulle@gmail.com>
2022-01-13 23:02:40 +0100

    UI : Add New button

--- jHackson.UI/ViewModels/MainViewModel.cs
+++ jHackson.UI/ViewModels/MainViewModel.cs
@@ -5,16 +5,36 @@
 namespace JHackson.UI.ViewModels
 {
     using System.Windows;
+    using JHackson.Core.Projects;
     using Prism.Commands;
 
     public class MainViewModel
     {
+        private IProjectJson project;
+
         public MainViewModel()
         {
+            this.NewCommand = new DelegateCommand(this.New);
             this.QuitCommand = new DelegateCommand(this.Quit);
         }
 
+        public DelegateCommand NewCommand { get; private set; }
+
         public DelegateCommand QuitCommand { get; private set; }
+
+        private void New()
+        {
+            if (this.project != null)
+            {
+                // TODO : Save the project
+
+                this.project = null;
+
+                // TODO : Close all UI elements
+            }
+
+            this.project = new ProjectJson();
+        }
 
         private void Quit()
         {
