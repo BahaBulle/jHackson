b774393f3076a5ca0127a029d0feacfb544a8175
BahaBulle <bahabulle@gmail.com>
2022-01-13 23:26:55 +0100

    UI : Add Save button

--- jHackson.UI/ViewModels/MainViewModel.cs
+++ jHackson.UI/ViewModels/MainViewModel.cs
@@ -6,21 +6,32 @@ namespace JHackson.UI.ViewModels
 {
     using System.Windows;
     using JHackson.Core.Projects;
+    using JHackson.Core.Services;
+    using Microsoft.Win32;
     using Prism.Commands;
 
     public class MainViewModel
     {
+        private readonly ISerializationService serializationService;
+
         private IProjectJson project;
 
-        public MainViewModel()
+        private string projectPath;
+
+        public MainViewModel(ISerializationService serializationService)
         {
+            this.serializationService = serializationService;
+
             this.NewCommand = new DelegateCommand(this.New);
             this.QuitCommand = new DelegateCommand(this.Quit);
+            this.SaveCommand = new DelegateCommand(this.Save);
         }
 
         public DelegateCommand NewCommand { get; private set; }
 
         public DelegateCommand QuitCommand { get; private set; }
+
+        public DelegateCommand SaveCommand { get; private set; }
 
         private void New()
         {
@@ -39,6 +50,39 @@ namespace JHackson.UI.ViewModels
         private void Quit()
         {
             Application.Current.MainWindow.Close();
+        }
+
+        private void Save()
+        {
+            bool saveConfirmed = true;
+
+            if (string.IsNullOrWhiteSpace(this.projectPath))
+            {
+                // TODO : Open select file dialog
+                var saveFileDialog = new SaveFileDialog
+                {
+                    FileName = this.project.Game,
+                    DefaultExt = ".json",
+                    Filter = "jHackson project (.json)|*.json"
+                };
+
+                bool? result = saveFileDialog.ShowDialog();
+
+                if (result == true)
+                {
+                    this.projectPath = saveFileDialog.FileName;
+                }
+                else
+                {
+                    saveConfirmed = false;
+                }
+            }
+
+            if (saveConfirmed)
+            {
+                this.project.Game = "test";
+                this.serializationService.Serialize(this.project, this.projectPath);
+            }
         }
     }
 }
