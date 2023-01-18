00984c76cb73f481db3d1ffb1923ab84a40a9d84
BahaBulle <bahabulle@gmail.com>
2023-01-18 23:04:29 +0100

    Add InternalsVisibleTo Tests

---
+++ Games/jHackson.Okamiden/Assembly.cs
@@ -0,0 +1,7 @@
+﻿// <copyright file="Assembly.cs" company="BahaBulle">
+//     Copyright (c) BahaBulle. All rights reserved.
+// </copyright>
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("jHackson.Okamiden.Tests")]
